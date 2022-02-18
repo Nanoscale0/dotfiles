f54d6e560975a4fbc5496c450a0d1ec831e2cff2
nanoscale0 <nanoscale@live.com>
2022-02-18 13:23:01 +1100

    first commit

---
+++ .config/polybar/colorblocks/scripts/window-switch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rofi -no-config -no-lazy-grab -kb-accept-entry '!Alt-Tab' -kb-row-down Alt-Tab -show window -modi window -theme ~/.config/polybar/colorblocks/scripts/rofi/launcher.rasi
